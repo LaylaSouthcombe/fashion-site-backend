33fb48c725ffd98aadd028aeabbd16c630854326
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-04-29 19:43:46 +0100

    add env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+require('dotenv').config()
 const app = express()
 app.use(express.json())
 // app.use(cors())
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 3000;
 
 
-mongoose.connect('mongodb+srv://laylasouthcombe789:O0YK6hmdILADdjKZ@portfolioprojectscluste.gs0gxnt.mongodb.net/FashionSite?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO_DB_URI)
 .then(() => {
   console.log('connected to mongoDB')
   app.listen(port, () => console.log(`Express departed from port ${port}`))
